9e842a69daf89adb5cdba648c4c5d672b0ff4a82
huangwenzi <35164459+huangwenzi@users.noreply.github.com>
2019-12-13 18:13:03 +0800

    添加描边

--- test.py
+++ test.py
@@ -4,11 +4,5 @@ import sys
 # 避免递归达到1000上限
 sys.setrecursionlimit(90000000)
 
-def run(idx):
-    idx += 1
-    print(idx)
-    if idx >= 100000:
-        return idx
-    return run(idx)
-
-run(1)
+for tmp_idx in range(8):
+    print(tmp_idx)
